2eca5ee510d4e38cc3df1e2d466fe10f2578ea73
hitechpioneer <kosiped831@rowplant.com>
2024-10-07 18:20:35 +0545

    fine tunning data

--- ObjectDetection/video_processor/video_capture.py
+++ ObjectDetection/video_processor/video_capture.py
@@ -1,9 +1,16 @@
 import cv2
 
-
 class VideoCapture:
-    def __init__(self, source=0):
+    def __init__(self, source=0, width=640, height=480, fps=15):
         self.capture = cv2.VideoCapture(source)
+        # Set video resolution
+        self.capture.set(cv2.CAP_PROP_FRAME_WIDTH, width)
+        self.capture.set(cv2.CAP_PROP_FRAME_HEIGHT, height)
+        # Set frame rate
+        self.capture.set(cv2.CAP_PROP_FPS, fps)
+
+        if not self.capture.isOpened():
+            raise ValueError(f"Error: Cannot open video source {source}")
 
     def get_frame(self):
         ret, frame = self.capture.read()
